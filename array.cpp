227ae391165ff980e1da43f78f9f9ecad08bb9f5
akmalropip <akmalropip@gmail.com>
2026-04-06 15:11:09 +0700

    mengganti index 1

--- array.cpp
+++ array.cpp
@@ -8,4 +8,9 @@ int main(){
     int kamu[5];
     //tampilkan data
     cout << "Data pada index 1 = " << aku[1] << endl;
+
+    //ganti index ke 1
+    aku[1] = 200;
+    cout << endl;
+    cout << "Data pada index 1 = " << aku[1] << endl;
 }
